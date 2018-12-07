346d2514453350799b9934afc6c8361182987cb4
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2018-12-07 21:56:16 +0000

    changing the greating message

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           welcome to kodflix
+           Hello Kodflix
           </p>
           <a
             className="App-link"
