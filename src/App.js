644d306c4580b72aa6a21a3fbb3d2ce00d2135bd
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2018-12-07 13:16:25 +0000

    fix the title message

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           wlcome to kodflix
+           welcome to kodflix
           </p>
           <a
             className="App-link"
