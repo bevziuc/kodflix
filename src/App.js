54f657d8afdae7c6a8b2029e6eeffc9a91085062
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2019-01-07 20:21:13 +0000

    last changes on third-task "Add 6 TV show titles"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import React, { Component } from 'react';
-// import bandOfBrothers from './img/tvShows/band-of-brothers.jpg';
-// import blackMirror from './img/tvShows/black-mirror.jpg';
-// import cosmos from './img/tvShows/cosmos.jpg';
-// import gameOfThrones from './img/tvShows/game-of-thrones.jpg';
-// import life from './img/tvShows/life.jpg';
-// import sopranos from './img/tvShows/the-sopranos.jpg';
 import './App.css';
 
 class App extends Component {
@@ -21,28 +15,6 @@ class App extends Component {
           <h4 className="TvShows__title-descr">Life</h4>
           <h4 className="TvShows__title-descr">Sopranos</h4>
         </div>
-        {/* <div className="TvShows__cover">
-          <div className='TvShows__img'>
-            <img src={bandOfBrothers} alt='Tv Shows Logo'/>
-          </div>
-          <div className='TvShows__img'>
-            <img src={blackMirror} alt='Tv Shows Logo'/>
-          </div>
-          <div className='TvShows__img'>
-            <img src={cosmos} alt='Tv Shows Logo'/>
-          </div>
-        </div>
-        <div className="TvShows__cover">
-          <div className='TvShows__img'>
-            <img src={gameOfThrones} alt='Tv Shows Logo'/>
-          </div>
-          <div className='TvShows__img'>
-            <img src={life} alt='Tv Shows Logo'/>
-          </div>
-          <div className='TvShows__img'>
-            <img src={sopranos} alt='Tv Shows Logo'/>
-          </div>
-        </div> */}
       </div>
     );
   }
