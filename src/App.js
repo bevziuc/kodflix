f13d415f37976067b617f6d4e37fd390fb756be1
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2019-01-07 16:34:12 +0000

    Add 6 TV show titles #3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import bandOfBrothers from './img/tvShows/band-of-brothers.jpg';
-import blackMirror from './img/tvShows/black-mirror.jpg';
-import cosmos from './img/tvShows/cosmos.jpg';
-import gameOfThrones from './img/tvShows/game-of-thrones.jpg';
-import life from './img/tvShows/life.jpg';
-import sopranos from './img/tvShows/the-sopranos.jpg';
+// import bandOfBrothers from './img/tvShows/band-of-brothers.jpg';
+// import blackMirror from './img/tvShows/black-mirror.jpg';
+// import cosmos from './img/tvShows/cosmos.jpg';
+// import gameOfThrones from './img/tvShows/game-of-thrones.jpg';
+// import life from './img/tvShows/life.jpg';
+// import sopranos from './img/tvShows/the-sopranos.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="Header">
-        <div className="TvShows__cover">
+        <div className="TvShows__title">
+          <h4 className="TvShows__title-descr">Band Of Brothers</h4>
+          <h4 className="TvShows__title-descr">Black Mirror</h4>
+          <h4 className="TvShows__title-descr">Cosmos</h4>
+        </div>
+        <div className="TvShows__title">
+          <h4 className="TvShows__title-descr">Game Of Thrones</h4>
+          <h4 className="TvShows__title-descr">Life</h4>
+          <h4 className="TvShows__title-descr">Sopranos</h4>
+        </div>
+        {/* <div className="TvShows__cover">
           <div className='TvShows__img'>
             <img src={bandOfBrothers} alt='Tv Shows Logo'/>
           </div>
@@ -32,7 +42,7 @@ class App extends Component {
           <div className='TvShows__img'>
             <img src={sopranos} alt='Tv Shows Logo'/>
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
