e4fd4e1def3b0ad1293d66c0ebd7a48b45a6eb0f
Daniel Bevziuc <37953629+bevziuc@users.noreply.github.com>
2019-01-08 21:24:50 +0000

    Add the TV show title overlay on hover-over #5 (#4)
    * Replace TV show titles with covers #2 "last changes"
    
    * Show a single TV show on each row for mobile devices #4
    
    * Add the TV show title overlay on hover-over #5
    
    * Add the TV show title overlay on hover-over #5 "added empty  space toseparate css"

--- src/App.js
+++ src/App.js
@@ -5,6 +5,45 @@ class App extends Component {
   render() {
     return (
       <div className="Header">
+        <div className="TvShows__cover">
+          <div className='TvShows__img'>
+            <img src={bandOfBrothers} alt='Tv Shows Logo'/>
+            <div className='TvShows__overlay'>
+              <h4>Band Of Brothers</h4>
+            </div>
+          </div>
+          <div className='TvShows__img'>
+            <img src={blackMirror} alt='Tv Shows Logo'/>
+            <div className='TvShows__overlay'>
+              <h4>Black Mirror</h4>
+            </div>
+          </div>
+          <div className='TvShows__img'>
+            <img src={cosmos} alt='Tv Shows Logo'/>
+            <div className='TvShows__overlay'>
+              <h4>Cosmos</h4>
+            </div>
+          </div>
+        </div>
+        <div className="TvShows__cover">
+          <div className='TvShows__img'>
+            <img src={gameOfThrones} alt='Tv Shows Logo'/>
+            <div className='TvShows__overlay'>
+              <h4>Game Of Thrones</h4>
+            </div>
+          </div>
+          <div className='TvShows__img'>
+            <img src={life} alt='Tv Shows Logo'/>
+            <div className='TvShows__overlay'>
+              <h4>Life</h4>
+            </div>
+          </div>
+          <div className='TvShows__img'>
+            <img src={sopranos} alt='Tv Shows Logo'/>
+            <div className='TvShows__overlay'>
+              <h4>Sopranos</h4>
+            </div>
+          </div>
         <div className="TvShows__title">
           <h4 className="TvShows__title-descr">Band Of Brothers</h4>
           <h4 className="TvShows__title-descr">Black Mirror</h4>
