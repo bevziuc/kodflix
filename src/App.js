d5f93a08679fcfe52eab5e0b6eed92b0dc542e86
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2019-01-08 11:46:21 +0000

    Add the TV show title overlay on hover-over #5

--- src/App.js
+++ src/App.js
@@ -14,23 +14,41 @@ class App extends Component {
         <div className="TvShows__cover">
           <div className='TvShows__img'>
             <img src={bandOfBrothers} alt='Tv Shows Logo'/>
+            <div className='TvShows__overlay'>
+              <h4>Band Of Brothers</h4>
+            </div>
           </div>
           <div className='TvShows__img'>
             <img src={blackMirror} alt='Tv Shows Logo'/>
+            <div className='TvShows__overlay'>
+              <h4>Black Mirror</h4>
+            </div>
           </div>
           <div className='TvShows__img'>
             <img src={cosmos} alt='Tv Shows Logo'/>
+            <div className='TvShows__overlay'>
+              <h4>Cosmos</h4>
+            </div>
           </div>
         </div>
         <div className="TvShows__cover">
           <div className='TvShows__img'>
             <img src={gameOfThrones} alt='Tv Shows Logo'/>
+            <div className='TvShows__overlay'>
+              <h4>Game Of Thrones</h4>
+            </div>
           </div>
           <div className='TvShows__img'>
             <img src={life} alt='Tv Shows Logo'/>
+            <div className='TvShows__overlay'>
+              <h4>Life</h4>
+            </div>
           </div>
           <div className='TvShows__img'>
             <img src={sopranos} alt='Tv Shows Logo'/>
+            <div className='TvShows__overlay'>
+              <h4>Sopranos</h4>
+            </div>
           </div>
         </div>
       </div>
