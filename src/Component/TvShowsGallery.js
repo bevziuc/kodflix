371a2a04d2d75139f8b1460a52921f5915a8adfe
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2019-01-11 11:45:37 +0000

    Move the covers data model to a proper front-end data source #12

--- src/Component/TvShowsGallery.js
+++ src/Component/TvShowsGallery.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import bandOfBrothers from '../img/tvShows/band-of-brothers.jpg';
-import blackMirror from '../img/tvShows/black-mirror.jpg';
-import cosmos from '../img/tvShows/cosmos.jpg';
-import gameOfThrones from '../img/tvShows/game-of-thrones.jpg';
-import life from '../img/tvShows/life.jpg';
-import sopranos from '../img/tvShows/the-sopranos.jpg';
+import getTehnologies from './get-tehnologies.js';
 import Cover from '../Component/Cover.js';
 import '../css/TvShowsGallery.css';
 
@@ -12,12 +7,15 @@ export default function TvShowsGallery(props) {
    return (
       <div className="Header">
         <div className="tvShows__cover">
-          <Cover id='band' name='Band Of Brothers' logo={bandOfBrothers} />
-          <Cover id='mirror' name='Black Mirror' logo={blackMirror} />
-          <Cover id='cosmos' name='Cosmos' logo={cosmos} />
-          <Cover id='thrones' name='Game Of Thrones' logo={gameOfThrones} />
-          <Cover id='life' name='Life' logo={life} />
-          <Cover id='sopranos' name='The Sopranos' logo={sopranos} />
+          {
+            getTehnologies().map(cover => (
+              <Cover
+                key={cover.id}
+                id={cover.id}
+                name={cover.name}
+                logo={cover.logo} />
+            ))
+          }
         </div>
       </div>
    );
