0da121370dbe712bedcc3ffdbe430edcb9891806
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2019-01-25 12:04:36 +0000

    Return a list of shows from the backend #19

--- src/back-end/app.js
+++ src/back-end/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const getMovies = require('./movies-details');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/rest/movies-details', (req, res) => res.send(getMovies()))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ src/back-end/movies-details.js
@@ -0,0 +1,36 @@
+let getMovies = function() {
+    return [
+        {
+            id: 'bandOfBrothers',
+            title: 'Band Of Brothers',
+            synopsis: "A 10-part miniseries based on Stephen Ambrose's bestseller about a World War II unit called Easy Company, 2nd Battalion of the 506th Regiment of the 101st Airborne Division, U.S. Army, that begins with their training in Georgia in the spring of 1942, follows them into France on D-Day and ends with the Allied victory in Europe in 1945."
+        },
+        {
+            id: 'mirror',
+            title: 'Black Mirror',
+            synopsis: "In 1984, a young programmer begins to question reality as he adapts a dark fantasy novel into a video game. A mind-bending tale with multiple endings."
+        },
+        {
+            id: 'cosmos',
+            title: 'Cosmos',
+            synopsis: "A young man, hoping to write a novel, visits a French guest-house with a friend, he but finds himself distracted by a strange mystery and the stranger inhabitants of the home."
+        },
+        {
+            id: 'gameOfThrones',
+            title: 'Game Of Thrones',
+            synopsis: "Nine noble families fight for control over the mythical lands of Westeros, while an ancient enemy returns after being dormant for thousands of years." 
+        },
+        {
+            id: 'life',
+            title: 'Life',
+            synopsis: "A team of scientists aboard the International Space Station discover a rapidly evolving life form that caused extinction on Mars and now threatens all life on Earth."
+        },
+        {
+            id: 'theSopranos',
+            title: 'The Sopranos',
+            synopsis: "New Jersey mob boss Tony Soprano deals with personal and professional issues in his home and business life that affect his mental state, leading him to seek professional psychiatric counseling."
+        },
+    ];
+}
+
+module.exports = getMovies;
