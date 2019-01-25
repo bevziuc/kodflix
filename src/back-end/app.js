a85bb74ac6e918edb983633a0930ec4971d2ce62
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2019-01-25 15:09:16 +0000

    Run frontend and backend servers in parallel #21

--- src/back-end/app.js
+++ src/back-end/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const getMovies = require('./movies-details');
 
 app.get('/rest/movies-details', (req, res) => res.send(getMovies()))
